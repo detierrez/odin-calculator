74d4da7d1ffa569b9de44fe5be48bb29abe3a1fc
detierrez <detierrez@gmail.com>
2025-05-10 01:56:26 -0400

    Make minus sign behavior when there's no number

--- script.js
+++ script.js
@@ -1,62 +1,93 @@
 import { round, operate } from "./math.js";
 
-const divMainDisplay = document.querySelector("#primary-display");
-const divMinorDisplay = document.querySelector("#secondary-display");
+const divBotDisplay = document.querySelector("#primary-display");
+const divTopDisplay = document.querySelector("#secondary-display");
 const btnEqual = document.querySelector("button[data-type=equal]");
 const btnClear = document.querySelector("button[data-type=clear]");
 const btnBackspace = document.querySelector("button[data-type=backspace]");
 const digitButtons = document.querySelectorAll("button[data-type=digit]");
 const operatorButtons = document.querySelectorAll("button[data-type=operator]");
 const allButtons = document.querySelectorAll("button");
+const btnMinus = document.querySelector("button[minus-sign]");
 
 const MAX_DIGITS = 10;
 
+btnBackspace.addEventListener("click", deleteDigit);
 btnClear.addEventListener("click", clearDisplay);
-btnBackspace.addEventListener("click", eraseDigit);
 btnEqual.addEventListener("click", enterEqual);
+
 digitButtons.forEach((btn) => {
   btn.addEventListener("click", enterDigit);
 });
+
 operatorButtons.forEach((btn) => {
   btn.addEventListener("click", enterOperator);
 });
+btnMinus.addEventListener("click", applySign);
+
 allButtons.forEach((btn) => {
   btn.addEventListener("click", updateDisplay);
 });
 
+let numberTop = null;
+let numberBot = null;
+let operator = "+";
+let isNegative = false;
+
+updateDisplay();
+
 function clearDisplay() {
   numberTop = null;
   numberBot = null;
+  isNegative = false;
 }
 
 function enterDigit(event) {
   let strDigit = event.target.textContent;
   if (numberBot === null) {
     numberBot = +strDigit;
+    if (isNegative) {
+      numberBot *= -1;
+      isNegative = false;
+    }
     return;
   }
+
   let strNumber = numberBot.toString();
   if (strNumber.length < MAX_DIGITS) {
     numberBot = +(strNumber + strDigit);
   }
 }
 
-function eraseDigit() {
-  if (numberBot === null) return;
-  let strNumber = numberBot.toString();
-  let isOneDigit = strNumber.length <= 1;
-  numberBot = isOneDigit ? null : +strNumber.slice(0, -1);
+function deleteDigit() {
+  if (numberBot === null) {
+    if (isNegative) {
+      isNegative = false;
+    }
+    return;
+  }
+
+  let slicedNumber = numberBot.toString().slice(0, -1);
+  if (slicedNumber !== "" && slicedNumber !== "-") {
+    numberBot = +slicedNumber;
+    return;
+  }
+
+  if (slicedNumber === "-") isNegative = true;
+  numberBot = null;
 }
 
 function enterEqual() {
   if (numberTop === null) return;
+
   if (numberBot === null) {
     numberBot = numberTop;
     numberTop = null;
-  } else {
-    numberBot = operate(numberTop, numberBot, operator);
-    numberTop = null;
+    return;
   }
+
+  numberBot = operate(numberTop, numberBot, operator);
+  numberTop = null;
 }
 
 function enterOperator(event) {
@@ -69,20 +100,23 @@ function enterOperator(event) {
     }
     return;
   }
+
   numberTop = operate(numberTop, numberBot, operator);
   numberBot = null;
   operator = pressedOperator;
 }
 
-function updateDisplay() {
-  console.log({ number1: numberTop, number2: numberBot, operator });
-  divMinorDisplay.textContent =
-    numberTop !== null ? `${numberTop} ${operator}` : "";
-  divMainDisplay.textContent = numberBot !== null ? `${numberBot}` : "";
+function applySign() {
+  if (numberTop === null && numberBot === null) {
+    isNegative = !isNegative;
+  }
 }
 
-let numberTop = 2;
-let numberBot = 0;
-let operator = "+";
+function updateDisplay() {
+  console.log({ number1: numberTop, number2: numberBot, operator });
+  divTopDisplay.textContent =
+    numberTop === null ? "" : `${numberTop} ${operator}`;
 
-updateDisplay();
+  let sign = isNegative ? "-" : "";
+  divBotDisplay.textContent = numberBot === null ? sign : `${numberBot}`;
+}
